f0791174234ceb691b3d8ce9b2c2df5c31123a0e
Sami <sami@qoala.id>
2019-10-08 21:45:09 +0700

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,11 @@ module.exports = {
   },
   extends: [
     'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'prettier/vue'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+  plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
